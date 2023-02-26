f89e590cadeb9d56929043a5472e20a24f5a32a4
nicolay <sirghiinicolai1@gmail.com>
2023-02-26 10:00:09 +0100

    found where is the problem

--- My_Tetris/script.js
+++ My_Tetris/script.js
@@ -26,67 +26,150 @@ buttonLeft.addEventListener("mouseover",()=>buttonLeft.setAttribute("class","fou
 buttonLeft.addEventListener('mouseout',()=>buttonLeft.setAttribute("class","buttonLeft"));
 buttonRight.addEventListener("mouseover",()=>buttonRight.setAttribute("class","fourButtons"));
 buttonRight.addEventListener('mouseout',()=>buttonRight.setAttribute("class","buttonRight"));
-
-let goesDownn = function(begin,end,speed,){
-    let score = begin - 1;
+/////////////////////////////////////////////////////////////
+let fun1 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ro+"c"+(co+1);
+    let c = ".r"+ro+"c"+(co+2);
+    let d = ".r"+ro+"c"+(co+3);
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "black";
+   two.style.backgroundColor = "black";
+   three.style.backgroundColor = "black";
+   four.style.backgroundColor = "black";
+};
+let fun2 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ro+"c"+(co+1);
+    let c = ".r"+ro+"c"+(co+2);
+    let d = ".r"+ro+"c"+(co+3);
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "rgb(140, 130, 130)";
+   two.style.backgroundColor = "rgb(140, 130, 130)";
+   three.style.backgroundColor = "rgb(140, 130, 130)";
+   four.style.backgroundColor = "rgb(140, 130, 130)";
+ };
+let fun3 = function(ro,co){
+    let d = ".r"+ro+"c"+co;
+    let c = ".r"+(ro+1)+"c"+co;
+    let b = ".r"+(ro+2)+"c"+co;
+    let a = ".r"+(ro+3)+"c"+co;
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "black";
+   two.style.backgroundColor = "black";
+   three.style.backgroundColor = "black";
+   four.style.backgroundColor = "black";
+};
+let fun4 = function(ro,co){
+    let d = ".r"+ro+"c"+co;
+    let c = ".r"+(ro+1)+"c"+co;
+    let b = ".r"+(ro+2)+"c"+co;
+    let a = ".r"+(ro+3)+"c"+co;
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "rgb(140, 130, 130)";
+   two.style.backgroundColor = "rgb(140, 130, 130)";
+   three.style.backgroundColor = "rgb(140, 130, 130)";
+   four.style.backgroundColor = "rgb(140, 130, 130)";
+ };
+let fun5 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ ro+"c"+(co+1);
+    let c = ".r"+(ro+1)+"c"+co;
+    let d = ".r"+(ro+1)+"c"+(co+1);
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "black";
+   two.style.backgroundColor = "black";
+   three.style.backgroundColor = "black";
+   four.style.backgroundColor = "black";
+};
+let fun6 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ ro+"c"+(co+1);
+    let c = ".r"+(ro+1)+"c"+co;
+    let d = ".r"+(ro+1)+"c"+(co+1);
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "rgb(140, 130, 130)";
+   two.style.backgroundColor = "rgb(140, 130, 130)";
+   three.style.backgroundColor = "rgb(140, 130, 130)";
+   four.style.backgroundColor = "rgb(140, 130, 130)";
+ };
+ let fun7 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ro+"c"+(co+1);
+    let c = ".r"+ro+"c"+(co+2);
+    let d = ".r"+(ro+1)+"c"+(co+2);
+    let e = ".r"+(ro-1)+"c"+(co+1);
+    let f = ".r"+(ro-1)+"c"+co;
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   let five = document.querySelector(e);
+   let six = document.querySelector(f);
+   one.style.backgroundColor = "black";
+   two.style.backgroundColor = "black";
+   three.style.backgroundColor = "black";
+   four.style.backgroundColor = "black";
+   five.style.backgroundColor = "rgb(140, 130, 130)";
+   six.style.backgroundColor = "rgb(140, 130, 130)";
+};
+let fun8 = function(ro,co){
+    let a = ".r"+ro+"c"+co;
+    let b = ".r"+ro+"c"+(co+1);
+    let c = ".r"+ro+"c"+(co+2);
+    let d = ".r"+(ro+1)+"c"+(co+2);
+    let one = document.querySelector(a);
+   let two = document.querySelector(b);
+   let three = document.querySelector(c);
+   let four = document.querySelector(d);
+   one.style.backgroundColor = "rgb(140, 130, 130)";
+   two.style.backgroundColor = "rgb(140, 130, 130)";
+   three.style.backgroundColor = "rgb(140, 130, 130)";
+   four.style.backgroundColor = "rgb(140, 130, 130)";
+ };
+/////////////////////////////////////////////////////////////////
+ let goesDownn = function(begin,end,speed){
+    let score = begin - 1;let coll = 1;
 setInterval(function(){if(score < end){ score += 1;
-console.log(".r"+score+"c3")
-fist(score,6);
-second((score-1),6);
-}},speed );
-};
-let goesDownn2 = function(begin,end,speed,){
-    let score = begin - 1;
+fun1(score,coll);fun2((score-1),coll);}},speed );};
+
+let goesDownn2 = function(begin,end,speed){
+    let score = begin - 1;let coll = 5;
 setInterval(function(){if(score < end){ score += 1;
-console.log(".r"+score+"c3")};
-fist(score,5);
-second(score-1,5)
-},speed );
-};
-let fist = function(r,c){
+fun3(score,coll);fun4((score-4),coll);}},speed );};
 
-    let one = ".r"+r+"c"+(c-1);
-     let two = ".r"+r+"c"+c;
-     let three = ".r"+(r+1)+"c"+c;
-     let four = ".r"+(1+r)+"c"+(1+c);
-   let g = document.querySelector(one);
-   let b = document.querySelector(two);
-   let a = document.querySelector(three);
-   let d = document.querySelector(four);
-   g.style.backgroundColor = "black";
-   b.style.backgroundColor = "black";
-   a.style.backgroundColor = "black";
-   d.style.backgroundColor = "black";
-};
-let second = function(r,c){
+let goesDownn3 = function(begin,end,speed){
+    let score = begin - 1;let coll = 6;
+setInterval(function(){if(score < end){ score += 1;
+fun5(score,coll);fun6((score-2),coll);}},speed );};
 
-        let one = ".r"+r+"c"+(c-1);
-         let two = ".r"+r+"c"+c;
-         let three = ".r"+r+"c"+c;
-         let four = ".r"+(1+r)+"c"+(1+c);
-       let g = document.querySelector(one);
-       let b = document.querySelector(two);
-       let a = document.querySelector(three);
-       let d = document.querySelector(four);
-       g.style.backgroundColor = "rgb(140, 130, 130)";
-       b.style.backgroundColor = "rgb(140, 130, 130)";
-       a.style.backgroundColor = "rgb(140, 130, 130)";
-       d.style.backgroundColor = "rgb(140, 130, 130)";
-    };
-reset.addEventListener('click',function(){goesDownn(1,19,500);setTimeout(()=>goesDownn2(1,17,500),3000);
+let goesDownn4 = function(begin,end,speed){
+    let score = begin - 1;let coll = 6;
+setInterval(function(){if(score < end){ score += 1;
+fun7(score,coll);fun8((score-2),coll);}},speed );};
+///////////////////////////////////////////////////////////////
+onOffButton.addEventListener("click",function(){
+    goesDownn(2,20,400,);
+goesDownn2(5,17,300,);
+goesDownn3(3,19,500,);
+setTimeout(()=>goesDownn4(3,18,600,),2000);
 });
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-    
